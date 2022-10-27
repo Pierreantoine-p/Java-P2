5014db462dbb0e9050b91ae0f836dc2df440b548
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-10-27 18:54:53 +0200

    readFile ok

--- Project_2/src/Application/readFile.java
+++ Project_2/src/Application/readFile.java
@@ -1,4 +1,23 @@
 package Application;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class readFile {
+    public static void main(String[] args) {
+        BufferedReader symptoms;
+        try {
+            symptoms = new BufferedReader(new FileReader(
+                    "symptoms.txt"));
+            String line = symptoms.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = symptoms.readLine();
+            }
+        } catch (IOException  e) {
+            e.printStackTrace();
+        }
+    }
 }
