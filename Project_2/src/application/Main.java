be93eaf61d79e5c9a9b617ecf393752168c42469
pierre-antoine.potage <pierre-antoine.potage@camera1regie.local>
2022-11-12 05:31:32 +0100

    working on write file

--- Project_2/src/application/Main.java
+++ Project_2/src/application/Main.java
@@ -2,22 +2,19 @@
 package application;
 
 
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         readFile fileRead = new readFile();
         count counter = new count();
-        //createDoc fileCreate = new createDoc();
-        //writingDoc fileWriting = new writingDoc();
+        writingDoc fileWriting = new writingDoc();
 
 
         fileRead.addFile("symptoms.txt");
-        List<String> list = fileRead.read();
-        counter.countSymptoms(list);
-        //fileCreate.nameFileOut("result.out");
-        //fileCreate.create();
-        //fileWriting.nameWriteFile("result.out");
-        //fileWriting.write();
+        List<String> listToCount = fileRead.read();
+        counter.countSymptoms(listToCount);
+        Map<String, Integer> fileToWritte = counter.countSymptoms(listToCount);
+        fileWriting.write(fileToWritte);
     }
 }
--- Project_2/src/application/writingDoc.java
+++ Project_2/src/application/writingDoc.java
@@ -1,23 +1,42 @@
 package application;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
 public class writingDoc {
-
-    private String nameFile;
-
-    public void nameWriteFile(String nameFile) {this.nameFile = nameFile;}
-
-    //par ordre alphabetique
-    public void write(){
-
-        try{
-            BufferedWriter writer = new BufferedWriter(new FileWriter(nameFile));
-            //writer.write();
+    /*public void create(){
+        try {
+            //Create new file
+            File fileOut = new File("result.out");
+            if (fileOut.createNewFile()){
+                System.out.println("create file: " + fileOut.getName());
+            }else{
+                System.out.println("File already exists.");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }*/
+    public void write(Map<String, Integer> fileToWritte){
+        try{
+            File fileOut = new File("result.out");
+
+            if (fileOut.createNewFile()){
+                System.out.println("create file: " + fileOut.getName());
+            }else{
+                System.out.println("File already exists.");
+            }
+            BufferedWriter writer = new BufferedWriter(new FileWriter("result.out"));
+            writer.write(fileToWritte.size());
+            writer.close();
+            System.out.println("in the doc" + writer);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
