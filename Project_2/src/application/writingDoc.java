57bb24f0ad15400d5c04ab3f2a90cefbc7fa8ee5
pierre-antoine.potage <pierre-antoine.potage@camera1regie.local>
2022-11-13 10:28:10 +0100

    all method on main

--- Project_2/src/application/writingDoc.java
+++ Project_2/src/application/writingDoc.java
@@ -1,42 +1,34 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Map;
+import java.util.Properties;
 
 public class writingDoc {
-    /*public void create(){
-        try {
-            //Create new file
-            File fileOut = new File("result.out");
-            if (fileOut.createNewFile()){
-                System.out.println("create file: " + fileOut.getName());
-            }else{
-                System.out.println("File already exists.");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
+
     public void write(Map<String, Integer> fileToWritte){
         try{
             File fileOut = new File("result.out");
-
-            if (fileOut.createNewFile()){
+            if (fileOut.createNewFile()) {
                 System.out.println("create file: " + fileOut.getName());
-            }else{
+            } else {
                 System.out.println("File already exists.");
             }
-            BufferedWriter writer = new BufferedWriter(new FileWriter("result.out"));
-            writer.write(fileToWritte.size());
-            writer.close();
-            System.out.println("in the doc" + writer);
 
+            Properties properties = new Properties();
+
+            for (Map.Entry<String,Integer> entry : fileToWritte.entrySet()) {
+                properties.put(entry.getKey(), entry.getValue());
+                System.out.println("getKey" + entry.getKey());
+                System.out.println("getValue" + entry.getValue());
+                System.out.println("entry" + entry);
+            }
+            properties.store(new FileOutputStream("result.out"), null);
+            System.out.println("properties" + properties);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 }
+
