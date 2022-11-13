deb06b3244921c2f9aa96eb837804440d01351cb
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-11-13 22:02:56 +0100

    method read file

--- Project_2/src/application/Main.java
+++ Project_2/src/application/Main.java
@@ -8,15 +8,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         readFile fileRead = new readFile();
-        count counter = new count();
-        //writingDoc fileWriting = new writingDoc();
+
 
         fileRead.addFile("symptoms.txt");
-        List<String> listToCount = fileRead.read();
-
-        counter.countSymptoms(listToCount);
-        //Map<String, Integer> fileToWrite = counter.countSymptoms(listToCount);
-        //fileWriting.write(fileToWrite);
 
     }
 }
--- Project_2/src/application/readFile.java
+++ Project_2/src/application/readFile.java
@@ -7,25 +7,28 @@ import java.util.ArrayList;
 
 public class readFile {
 
-    //Name file who need read
+    //Name file read
     private static String nameFile;
-    //Add file who need read in method read
+    //Method add name file  read
     public void addFile(String nameFile) {
         readFile.nameFile = nameFile;
     }
 
     public ArrayList<String> read() {
         BufferedReader file;
-        //Read line file
+
         ArrayList<String> stringArray = new ArrayList<>();
         try {
+            // Path file read
             file = new BufferedReader(new FileReader(
                     nameFile));
+            // init method read file
             String line = file.readLine();
             while (line != null) {
+                //Read line file
                 line = file.readLine();
+                //Add line in stringArray
                 stringArray.add(line);
-
             }
         } catch (IOException e) {
             e.printStackTrace();
