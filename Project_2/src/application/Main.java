b7bb35b8b53a8fcc9769fbb1c260cb008c39ce62
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-11-13 22:07:07 +0100

    method count

--- Project_2/src/application/Main.java
+++ Project_2/src/application/Main.java
@@ -1,22 +1,15 @@
-
-
 package application;
-
-
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Init method
         readFile fileRead = new readFile();
         count counter = new count();
-        //writingDoc fileWriting = new writingDoc();
 
+        // Catch return methods
         fileRead.addFile("symptoms.txt");
         List<String> listToCount = fileRead.read();
-
         counter.countSymptoms(listToCount);
-        //Map<String, Integer> fileToWrite = counter.countSymptoms(listToCount);
-        //fileWriting.write(fileToWrite);
-
     }
 }
