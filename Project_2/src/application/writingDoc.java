8a7311ac2d0258d5ded5dd2ef0919cb6d0dc2d19
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-11-14 07:49:01 +0100

    Final

--- Project_2/src/application/writingDoc.java
+++ Project_2/src/application/writingDoc.java
@@ -8,20 +8,23 @@ public class writingDoc {
 
     public void write(Map<String, Integer> fileToWrite){
         try{
+            //Create file
             File fileOut = new File("result.out");
             if (fileOut.createNewFile()) {
                 System.out.println("create file: " + fileOut.getName());
             } else {
                 System.out.println("File already exists.");
             }
+            //Write in file
             BufferedWriter writingFile = new BufferedWriter(new FileWriter(fileOut));
             for (Map.Entry<String,Integer> entry : fileToWrite.entrySet()) {
+                //Write key : value
                 writingFile.write(entry.getKey() + ":"
                         + entry.getValue());
-                System.out.println("getKey" + entry.getKey());
-                System.out.println("getValue" + entry.getValue());
+                //Add line
                 writingFile.newLine();
             }
+            //Write close
             writingFile.flush();
         } catch (IOException e) {
             e.printStackTrace();
